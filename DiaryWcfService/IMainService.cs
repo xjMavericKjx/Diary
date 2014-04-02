61f8c6b1d60493135bc683869c5d99ff114f5d5e
BorodinMS <borm@umssoft.com>
2014-04-02 14:22:21 +0700

    change name of service

---
+++ DiaryWcfService/IMainService.cs
@@ -0,0 +1,15 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace DiaryWcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IMainService" in both code and config file together.
+    [ServiceContract]
+    public interface IMainService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "PUT", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json,
+           UriTemplate = "/Organisation", BodyStyle = WebMessageBodyStyle.Wrapped)]
+        void DoWork();
+    }
+}
---
+++ DiaryWcfService/MainService.svc.cs
@@ -0,0 +1,11 @@
+﻿namespace DiaryWcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MainService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select MainService.svc or MainService.svc.cs at the Solution Explorer and start debugging.
+    public class MainService : IMainService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
